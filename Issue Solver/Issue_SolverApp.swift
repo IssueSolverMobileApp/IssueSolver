cea8d954d5bc4ddcaa944eb0f9847ce250eefc74
Iradabek <iradebekirli@gmail.com>
2024-06-11 11:21:06 +0400

    Project added

---
+++ Issue Solver/Issue_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Issue_SolverApp.swift
+//  Issue Solver
+//
+//  Created by Irada Bakirli on 11.06.24.
+//
+
+import SwiftUI
+
+@main
+struct Issue_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
