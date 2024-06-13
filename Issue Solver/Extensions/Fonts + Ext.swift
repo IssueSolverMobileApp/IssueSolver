0b2e49db8f2909d5756a05452ca9962998a21259
Iradabek <iradebekirli@gmail.com>
2024-06-13 15:50:13 +0400

    functions for fonts created like extension

---
+++ Issue Solver/Extensions/Fonts + Ext.swift
@@ -0,0 +1,48 @@
+//
+//  Fonts + Ext.swift
+//  Issue Solver
+//
+//  Created by Irada Bakirli on 13.06.24.
+//
+
+import SwiftUI
+
+extension Font {
+    static func plusJakartaSansBold(size: CGFloat) -> Font {
+        return Font ("PlusJakartaSans-Bold" as! CTFont)
+    }
+    
+    static func plusJakartaSansSemiBold(size: CGFloat) -> Font {
+        return Font ("PlusJakartaSans-SemiBold" as! CTFont)
+    }
+    
+    static func plusJakartaSansExtraBold(size: CGFloat) -> Font {
+        return Font ("PlusJakartaSans-ExtraBold" as! CTFont)
+    }
+    
+    static func plusJakartaSansLight(size: CGFloat) -> Font {
+        return Font ("PlusJakartaSans-Light" as! CTFont)
+    }
+    
+    static func plusJakartaSansExtraLight(size: CGFloat) -> Font {
+        return Font ("PlusJakartaSans-ExtraLight" as! CTFont)
+    }
+    
+    static func plusJakartaSansItalic(size: CGFloat) -> Font {
+        return Font ("PlusJakartaSans-Italic" as! CTFont)
+    }
+    
+    static func plusJakartaSansMedium(size: CGFloat) -> Font {
+        return Font ("PlusJakartaSans-Medium" as! CTFont)
+    }
+    
+    static func plusJakartaSansRegular(size: CGFloat) -> Font {
+        return Font ("PlusJakartaSans-Regular" as! CTFont)
+    }
+
+    
+}
+
+
+
+
