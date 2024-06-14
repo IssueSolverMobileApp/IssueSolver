96e6c646ea1eb42537f4f44da80a53177a050ad5
Iradabek <iradebekirli@gmail.com>
2024-06-14 09:57:48 +0400

    EmailVerificationView created

--- Issue Solver/UI/Screens/Authentication/LoginView/LoginView.swift
+++ Issue Solver/UI/Screens/Authentication/LoginView/LoginView.swift
@@ -14,13 +14,14 @@ struct LoginView: View {
         ZStack {
             Color.surfaceBackground.ignoresSafeArea()
             
-            VStack {
+            VStack(alignment: .leading, spacing: 32) {
                 titleView
                 textFieldsView
                 Spacer()
                 loginButtonView
             }
             .padding(.top, 24)
+            .padding(.horizontal, 16)
         }
     }
     
@@ -43,7 +44,6 @@ struct LoginView: View {
                 
             }
         }
-        .padding()
     }
     
     var loginButtonView: some View {
