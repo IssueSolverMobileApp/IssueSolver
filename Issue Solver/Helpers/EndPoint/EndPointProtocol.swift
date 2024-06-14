58ad40252932d5b8a591f814c2bb57efe03d7a04
Valeh Amirov <amirovaleh@gmail.com>
2024-06-14 15:23:46 +0400

    Endpoints+JSONConverter (decode encode) HTTPAuthRepository

---
+++ Issue Solver/Helpers/EndPoint/EndPointProtocol.swift
@@ -0,0 +1,21 @@
+//
+//  EndPointProtocol.swift
+//  Issue Solver
+//
+//  Created by ValehAmirov on 14.06.24.
+//
+
+import Foundation
+
+protocol EndPointProtocol {
+    var baseURL: String { get }
+    var url: String { get }
+}
+
+extension EndPointProtocol {
+    
+    var baseURL: String {
+        return "https://govermentauthapi20240610022027.azurewebsites.net/api/"
+    }
+
+}
---
+++ Issue Solver/Helpers/Repository/HTTPAuthRepository.swift
@@ -0,0 +1,69 @@
+//
+//  HTTPAuthRepository.swift
+//  Issue Solver
+//
+//  Created by ValehAmirov on 14.06.24.
+//
+
+import Foundation
+import NetworkingPack
+
+class HTTPAuthRepository {
+    
+    private var http: HTTPClient = .shared
+    
+    func register(body: RegisterModel) async throws -> ErrorModel? {
+        do {
+            return try await http.POST(endPoint: EndPoint.auth(.register).url, body: JSONConverter().encode(input: body))
+        }
+        catch {
+            throw error
+        }
+    }
+     
+    
+    func login(body: LoginModel) async throws -> ErrorModel? {
+        do {
+            return try await http.POST(endPoint: EndPoint.auth(.login).url, body: JSONConverter().encode(input: body))
+        }
+        catch {
+            throw error
+        }
+    }
+    
+    func confirmOTP(body: OTPModel) async throws -> ErrorModel? {
+        do {
+            return try await http.GET(endPoint: EndPoint.auth(.confirmOTP).url)
+        }
+        catch {
+            throw error
+        }
+    }
+    
+    func resendOTP(body: EmailModel) async throws -> ErrorModel? {
+        do {
+            return try await http.POST(endPoint: EndPoint.auth(.resendOTP).url, body: JSONConverter().encode(input: body))
+        }
+        catch {
+            throw error
+        }
+    }
+    
+    func forgetPassword(body: EmailModel) async throws -> ErrorModel? {
+        do {
+            return try await http.POST(endPoint: EndPoint.auth(.forgetPassword).url, body: JSONConverter().encode(input: body))
+        }
+        catch {
+            throw error
+        }
+    }
+    
+    func resetPassword(body: ResetPasswordModel) async throws -> ErrorModel? {
+        do {
+            return try await http.POST(endPoint: EndPoint.auth(.resetPassword).url, body: JSONConverter().encode(input: body))
+        }
+        catch {
+            throw error
+        }
+    }
+}
