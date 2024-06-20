bcb069162a3d992ce7d01c32b5f53ad309199574
Valeh Amirov <amirovaleh@gmail.com>
2024-06-20 12:01:23 +0400

    created new branch,updated Success and ErrorModel

--- NetworkingPack/Sources/NetworkingPack/Model/ErrorModel.swift
+++ NetworkingPack/Sources/NetworkingPack/Model/ErrorModel.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 struct ErrorModel: Codable {
-    let message: String
+    let timeSpam: String?
+    let url: String?
+    let title: String?
+    let success: Bool?
+    let status: Int?
+    let message: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case timeSpam = "TimeSpam"
+        case url = "Url"
+        case title = "Title"
+        case success = "Success"
+        case status = "Status"
+        case message = "Message"
+    }
 }
