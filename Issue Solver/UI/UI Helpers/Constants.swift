ba63da3b5769058bc7d1c4222249d66d7e925f55
vusal <zevs.1418@gmail.com>
2024-06-24 20:20:20 +0400

    TextView Implementation
    Created text view, where you can also add clickable strings.

--- Issue Solver/UI/UI Helpers/Constants.swift
+++ Issue Solver/UI/UI Helpers/Constants.swift
@@ -9,4 +9,8 @@ import SwiftUI
 
 class Constants {
     static let cornerRadius: CGFloat = 12
+    
+    /// - Clickable Texts and their url strings
+    static let termsOfUse: [String: String] = ["İstifadəçi şərtləri": "https://www.youtube.com/watch?v=82Afo639J-0"]
+    static let privacyPolicy: [String: String] = ["Məxfilik siyasəti": "https://stackoverflow.com/questions/39238366/uitextview-with-hyperlink-text"]
 }
