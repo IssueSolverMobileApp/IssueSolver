f85cf2a48875fe65326fac453fcf7313090c1e25
Valeh Amirov <amirovaleh@gmail.com>
2024-07-05 08:31:21 +0400

    created view and viewModel

---
+++ Issue Solver/UI/Screens/MyRequest/ViewModels/MyRequestViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MyRequestViewModel.swift
+//  Issue Solver
+//
+//  Created by Valeh Amirov on 05.07.24.
+//
+
+import Foundation
+
+class MyRequestViewModel: ObservableObject {
+    
+    
+    
+}
---
+++ Issue Solver/UI/Screens/MyRequest/Views/MyRequestView.swift
@@ -0,0 +1,20 @@
+//
+//  MyRequestView.swift
+//  Issue Solver
+//
+//  Created by Valeh Amirov on 05.07.24.
+//
+
+import SwiftUI
+
+
+struct MyRequestView: View {
+    
+    @StateObject var vm = MyRequestViewModel()
+    
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
