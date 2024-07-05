703d9e9240a16b890cc954c1780a30bb92ebdc09
Valeh Amirov <amirovaleh@gmail.com>
2024-07-05 10:15:29 +0400

    created postView

--- Issue Solver/UI/Screens/MyRequest/ViewModels/MyRequestViewModel.swift
+++ Issue Solver/UI/Screens/MyRequest/ViewModels/MyRequestViewModel.swift
@@ -9,6 +9,6 @@ import Foundation
 
 class MyRequestViewModel: ObservableObject {
     
-    
+    @Published var postText: String = "Office ipsum you must be muted. Teeth recap latest didn't at. Innovation hill as wider assassin heads-up stronger give. Who's cloud low out email later charts. Believe our territories good client incentivization decisions pole product with. Pushback like be reach incompetent. Need bake ditching another loss to. Algorithm now pants items future-proof needle elephant i'm synergize old. Optimize meat room dog board invested devil reach. Horse building more prioritize meat per stakeholders.building"
     
 }
--- Issue Solver/UI/Screens/MyRequest/Views/MyRequestView.swift
+++ Issue Solver/UI/Screens/MyRequest/Views/MyRequestView.swift
@@ -13,8 +13,52 @@ struct MyRequestView: View {
     @StateObject var vm = MyRequestViewModel()
     
     var body: some View {
-        VStack {
-            
+        ZStack {
+            Color.surfaceBackground.ignoresSafeArea()
+            ScrollView  {
+                VStack {
+                    titleView
+                    postView
+                }
+                .padding(.horizontal, 20)
+                .padding(.bottom, 16)
+            }
         }
     }
+    
+    
+    
+    
+    var titleView: some View {
+        CustomTitleView(title: "Mənim sorğularım")
+    }
+    
+    var postView: some View {
+        VStack {
+            HStack {
+                Image(.profileIcon)
+                Text("Aynur Qəmbərova")
+                    .jakartaFont(.subtitle)
+                    .foregroundStyle(.primaryBlue)
+                Spacer()
+                HStack {
+                    Image(.blueDotIcon)
+                    Text("Gözləmədə")
+                        .jakartaFont(.subheading)
+                        .foregroundStyle(Color.primaryBluePressed)
+                }
+                .padding([.leading, .trailing], 20)
+                .padding([.top, .bottom], 8)
+                .background(.primaryBlue.opacity(0.28))
+                .clipShape(.rect(cornerRadius: 100))
+            }
+            
+            Text(vm.postText)
+
+        }
+    }
+}
+
+#Preview {
+    MyRequestView()
 }
