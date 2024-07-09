57e7ea5b739422e8f0de8207f135167a39d03991
Valeh Amirov <amirovaleh@gmail.com>
2024-07-09 10:48:24 +0400

    Added new extension and modifier(AdaptasionToKeyboard) to view + QueryModel + MyQueryEndPoint + HTTPMyQueryRepository . Refactoring on QueryCommentBottomSheetView

--- Issue Solver/Extensions/View + Ext.swift
+++ Issue Solver/Extensions/View + Ext.swift
@@ -50,3 +50,16 @@ extension View {
         
     }
 }
+
+/// this extension for TextEditor background color
+extension View {
+    func textEditorBackground(_ content: Color) -> some View {
+        if #available(iOS 16.0, *) {
+            return self.scrollContentBackground(.hidden)
+                .background(content)
+        } else {
+            UITextView.appearance().backgroundColor = .clear
+            return self.tint(content)
+        }
+    }
+}
--- NetworkingPack/Sources/NetworkingPack/EndPoint/EndPointCore/EndPoint.swift
+++ NetworkingPack/Sources/NetworkingPack/EndPoint/EndPointCore/EndPoint.swift
@@ -11,6 +11,7 @@ public enum EndPoint: EndPointProtocol {
     
     case auth(AuthEndPoint)
     case profile(ProfileEndPoint)
+    case myQuery(MyQueryEndPoint)
     
     var url: String {
         switch self {
@@ -18,17 +19,24 @@ public enum EndPoint: EndPointProtocol {
             return authEndPoint.url
         case .profile(let profileEndPoint):
             return profileEndPoint.url
+        case .myQuery(let myQueryEndPoint):
+            return myQueryEndPoint.url
         }
     }
 }
 
 protocol EndPointProtocol {
     var baseURL: String { get }
+    var baseRequestMS: String { get }
 }
 
 extension EndPointProtocol {
     
     var baseURL: String {
         return "https://govermentauthapi20240610022027.azurewebsites.net/api/"
+    }
+    
+    var baseRequestMS: String {
+        return "https://request-ms-d25203bd24ff.herokuapp.com/"
     }
 }
